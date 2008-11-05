c4d8bb7a7dc3210062381719eac9f425eecd412e
crazybenny <crazybenny@baf66a90-84c5-4322-ab86-c872dcac9201>
2008-11-05 15:55:30 +0000

    added button tooltip, here we go release!

--- portals.lua
+++ portals.lua
@@ -211,9 +211,32 @@ function frame:SKILL_LINES_CHANGED()
 	UpdateSpells()
 end
 
+-- All credit for this func goes to Tekkub and his picoGuild!
+local function GetTipAnchor(frame)
+		local x,y = frame:GetCenter()
+		if not x or not y then return "TOPLEFT", "BOTTOMLEFT" end
+		local hhalf = (x > UIParent:GetWidth()*2/3) and "RIGHT" or (x < UIParent:GetWidth()/3) and "LEFT" or ""
+		local vhalf = (y > UIParent:GetHeight()/2) and "TOP" or "BOTTOM"
+		return vhalf..hhalf, frame, (vhalf == "TOP" and "BOTTOM" or "TOP")..hhalf
+end
+
 function obj.OnClick(self, button)
 	if button == "RightButton" then
 		dewdrop:Open(self, "children", function() UpdateMenu() end)
 	end
 end
 
+function obj.OnLeave() 
+	GameTooltip:Hide() 
+end
+
+function obj.OnEnter(self)
+ 	GameTooltip:SetOwner(self, "ANCHOR_NONE")
+	GameTooltip:SetPoint(GetTipAnchor(self))
+	GameTooltip:ClearLines()
+
+	GameTooltip:AddLine("Broker Portals")
+	GameTooltip:AddDoubleLine(L["Right-Click"], L["to see list of spells"], 0.9, 0.6, 0.2, 0.2, 1, 0.2)
+
+	GameTooltip:Show()
+end
