7c2116d0074ae604110a112c8f4c0031b916cf05
crazybenny <crazybenny@baf66a90-84c5-4322-ab86-c872dcac9201>
2008-11-06 23:40:36 +0000

    updated deDE locale (courtesy of WalleniuM)

--- locale-deDE.lua
+++ locale-deDE.lua
@@ -2,8 +2,8 @@ local L = LibStub("AceLocale-3.0"):NewLocale("Broker_Portals", "deDE", true)
 
 if L then
 	L["Inn: "] 					= "Gasthaus: "
-	L["Right-Click"] 			= "Rechtsklick"
-	L["to see list of spells"] 	= "für Liste der Portalzauber"
+	L["Right-Click"] 			= "Rechtsklicken"
+	L["to see list of spells"] 	= "um Liste mit Zaubern zu sehen"
 	L["Hearthstone"] 			= "Ruhestein"
-	L["Attach to minimap"]		= true
+	L["Attach to minimap"]		= "An Minimap anhängen"
 end
