9a167de29ff46f1b3ca6f021704f0d7270fa75a4
crazybenny <crazybenny@baf66a90-84c5-4322-ab86-c872dcac9201>
2010-08-28 11:40:16 +0000

    added esMX and zhCN locales

---
+++ locale-esMX.lua
@@ -0,0 +1,7 @@
+if GetLocale() == "esMX" then
+  local addonName, addonTable = ...
+  local L = addonTable.L
+
+--@localization(locale="esMX", format="lua_additive_table", handle-unlocalized="comment")@
+
+end
---
+++ locale-zhCN.lua
@@ -0,0 +1,8 @@
+if GetLocale() == "zhCN" then
+  local addonName, addonTable = ...
+  local L = addonTable.L
+
+--@localization(locale="zhCN", format="lua_additive_table", handle-unlocalized="comment")@
+
+end
+
