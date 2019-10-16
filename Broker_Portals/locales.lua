2dfa43c73d6f813fde839f203bceeac25627162f
Blazeflack <blazeflack@hotmail.com>
2019-10-16 21:43:39 +0200

    Moved all files into subdirectory for easy manual install

---
+++ Broker_Portals/locales.lua
@@ -0,0 +1,65 @@
+local addonName, addonTable = ...
+local L = {}
+addonTable.L = L
+
+--@localization(locale="enUS", format="lua_additive_table")@
+
+if GetLocale() == "ptBR" then
+
+--@localization(locale="ptBR", format="lua_additive_table")@
+
+end
+
+if GetLocale() == "frFR" then
+
+--@localization(locale="frFR", format="lua_additive_table")@
+
+end
+
+if GetLocale() == "deDE" then
+
+--@localization(locale="deDE", format="lua_additive_table")@
+
+end
+
+if GetLocale() == "itIT" then
+
+--@localization(locale="itIT", format="lua_additive_table")@
+
+end
+
+if GetLocale() == "koKR" then
+
+--@localization(locale="koKR", format="lua_additive_table")@
+
+end
+
+if GetLocale() == "esMX" then
+
+--@localization(locale="esMX", format="lua_additive_table")@
+
+end
+
+if GetLocale() == "ruRU" then
+
+--@localization(locale="ruRU", format="lua_additive_table")@
+
+end
+
+if GetLocale() == "zhCN" then
+
+--@localization(locale="zhCN", format="lua_additive_table")@
+
+end
+
+if GetLocale() == "esES" then
+
+--@localization(locale="esES", format="lua_additive_table")@
+
+end
+
+if GetLocale() == "zhTW" then
+
+--@localization(locale="zhTW", format="lua_additive_table")@
+
+end
